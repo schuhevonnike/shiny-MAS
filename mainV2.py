f6c9dbc0341ff94e7db73226c4e31e06229d6dba
schreinn710 <niklasschreiner.ns@gmail.com>
2024-08-22 18:33:31 +0200

    update

--- mainV2.py
+++ mainV2.py
@@ -1,5 +1,18 @@
 import torch
 import pandas
+import os
+import
 
 
-def run_experiment():
+# Experiment parameters
+agent_types = ['adversary', 'cooperator']
+# ...
+
+# Cuda cores are better suited for such tasks, so ideally, we want to run the program on a cuda. If not possible, use the standard CPU.
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+def run_experiment(env):
+    # Initialize separate dictionaries for adversary and cooperator agents
+    adversary_agents = {}
+    cooperator_agents = {}
+
