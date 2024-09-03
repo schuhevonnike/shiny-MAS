30f5025ed082077d3a9ccefc357c127e2fb7ef9b
schreinn710 <niklasschreiner.ns@gmail.com>
2024-09-03 17:35:28 +0200

    update

--- environments/pettingzoo_env2.py
+++ environments/pettingzoo_env2.py
@@ -22,6 +22,21 @@ if __name__ == "__main__":
     done = False
     while not done:
         for agent in env.agent_iter():
+            observation, reward, termination, truncation, _ = env.last()
+            last_observations[agent] = observation  # Store the last observation
+            print(f"Last observations' shape for {agent}: {observation.shape}")    # Print the shape of the last observation
+
+            if termination or truncation:
+                action = None
+            else:
+                action = env.action_space(agent).sample() # Sample a random action
+
+            next_observation, reward, done, info = env.step(action) # Execute the action and store the according values
+
+            # Log the step result
+            print(f"Step result: {next_observation}, {reward}, {done}, {info}")
+
+        for agent in env.agent_iter():
             # Retrieve the recent observation, reward, termination, truncation, and info for the current agent
             observation, reward, termination, truncation, info = env.last()
             last_observations[agent] = observation  # Store the last observation
